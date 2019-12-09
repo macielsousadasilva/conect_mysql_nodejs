de68424ed2bf245dd8c3703ab05ab3636be1bb8d
macielmmx <48188441+macielsousadasilva@users.noreply.github.com>
2019-12-09 18:57:20 -0300

    Conexão com mysql através da biblioteca sequelize
    Para tudo funcionar e necessário utilizar:
    
    Express.
    Na pasta do projeto instale o express através dos comandos
    $ npm install express --save
    
    Sequelize
    Na pasta do projeto instale o sequelize através dos comandos
    
    $ npm install --save sequelize 
     
    Após isso instale de acordo com o banco de dados que ira utilizar no nosso caso foi o mysql
    $ npm install --save pg pg-hstore # Postgres 
    $ npm install --save mysql2
    $ npm install --save mariadb
    $ npm install --save sqlite3
    $ npm install --save tedious # Microsoft SQL Server

---
+++ connect.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+
+//Cria os parametros de conexão
+const sequelize = new Sequelize('db_nodejs','usuario','senha', {
+    host: "192.168.64.3",//localhost
+    dialect: "mariadb",
+    dialectOptions: {connectTimeout: 1000}
+})
+
+//Verifica se está conectado
+sequelize.authenticate().then(function(){
+    console.log('Conectado')
+}).catch(function(erro){
+    console.log("erro "+ erro)
+})
+
+//Cria a tabela <postagens> com as colunas <titulo> , <conteudo>
+const Postagens = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+//Cria a tabela <usuarios> com as colunas <nome>, <sobrenome>, <idade>, <email>
+const Usuarios = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+//Ao executar as linhas abaixo ira automaticamente criar as tabelas, apos criar comente.
+Postagens.sync({ force: true})
+Usuarios.sync({ force: true})
+
+//Faz um Insert com os dados <titulo> , <conteudo>
+Postagens.create({
+    titulo: 'Um novo titulo',
+    conteudo: "Esse e um novo conteudo"
+})
